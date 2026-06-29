153ab71a0519024c4fac79494e0b442b7252822f
zwanss <cotsu880@gmail.com>
2026-06-29 20:01:01 +0700

    menambahkan api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,14 +4,20 @@ use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',  // <-- TAMBAHKAN INI
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->api(prepend: [
+            EnsureFrontendRequestsAreStateful::class,  // <-- TAMBAHKAN INI
+        ]);
+
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
